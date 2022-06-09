4b924b78233d375054a3f78989a2384d0769fd25
KhalilurRahmanKhan <khanjahid373@gmail.com>
2022-06-09 17:56:05 +0600

    add email functionality

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,7 +1,22 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 
 function Contact() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_soeluyp', 'template_o10neec', form.current, 'user_hUriYfpRBIE4YNyyv6WTM')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
     return (
         <div>
         <Navbar></Navbar>
@@ -14,24 +29,31 @@ function Contact() {
           </div>
           <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <div class="card-body">
-              <div class="form-control">
+                <form ref={form} onSubmit={sendEmail}>
+                <div class="form-control">
+                <label class="label">
+                  <span class="label-text">name</span>
+                </label>
+                <input type="text" placeholder="name"  name="user_name" class="input input-bordered" />
+              </div>
+                <div class="form-control">
                 <label class="label">
                   <span class="label-text">Email</span>
                 </label>
-                <input type="text" placeholder="email" class="input input-bordered" />
+                <input type="text" placeholder="email"  name="user_email" class="input input-bordered" />
               </div>
               <div class="form-control">
                 <label class="label">
-                  <span class="label-text">Password</span>
+                  <span class="label-text">Comments</span>
                 </label>
-                <input type="text" placeholder="password" class="input input-bordered" />
-                <label class="label">
-                  <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
-                </label>
+                <textarea placeholder="Comments" name="message"  class="input input-bordered"></textarea>
+             
               </div>
               <div class="form-control mt-6">
-                <button class="btn btn-primary">Login</button>
+              <input className="btn btn-primary" type="submit" value="Send" />
               </div>
+                </form>
+          
             </div>
           </div>
         </div>
